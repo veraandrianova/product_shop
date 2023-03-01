2866672d01e93d9019b2630ac78d59647a9d614b
vera <lerika250293@mail.ru>
2023-03-01 21:23:34 +0300

    add attrs

--- user/serializers.py
+++ user/serializers.py
@@ -1,5 +1,5 @@
 from django.core.exceptions import ValidationError
-from djoser.serializers import UserCreatePasswordRetypeSerializer
+from djoser.serializers import UserCreatePasswordRetypeSerializer, UserSerializer
 from rest_framework import serializers
 
 from .models import User
@@ -7,12 +7,18 @@ from .validators import validate_password
 
 
 class UsersCreateSerializer(UserCreatePasswordRetypeSerializer):
-    """Serialization to create user data"""
+    """Сериализатор регистрации"""
     email = serializers.EmailField(required=True)
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'middle_name', 'email', 'phone', 'password')
+        fields = ('first_name', 'last_name', 'middle_name', 'email', 'phone', 'password')
+
+    def validate(self, attrs):
+        username = f'{attrs["phone"]}'
+        attrs['username'] = username
+        attrs = super().validate(attrs)
+        return attrs
 
     def validate_password(self, value):
         try:
